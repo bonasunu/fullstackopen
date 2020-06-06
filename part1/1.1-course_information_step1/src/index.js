84c92197300a8dd11c2d69fb392efd37de296d15
Bonaventura Sunu <bonaventurasunu@gmail.com>
2020-06-06 16:45:15 +0700

    Add files via upload

---
+++ part1/1.1-course_information_step1/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  );
+}
+
+const Header = (props) => {
+  return (
+    <>
+    <h1>{props.course}</h1>
+    </>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <>
+    <p>{props.part} {props.exercise}</p>
+    </>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <>
+    <p>Number of exercises {props.total}</p>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
