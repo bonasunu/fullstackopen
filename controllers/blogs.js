e6d3f148f22c009c04aa2f3340ced6e716038f88
Bonaventura Sunu <bonaventurasunu@gmail.com>
2020-06-21 21:58:36 +0700

    Add files via upload

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,36 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const blog = require('../models/blog')
+const { request } = require('../app')
+const { response } = require('express')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
   
-blogRouter.post('/', async (request, response, next) => {
-const blog = new Blog(request.body)
+blogRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
 
-const savedBlog = await blog.save()
-response.json(savedBlog.toJSON())
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
 
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id.toString())
+  response.status(204).end()
+})
 
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
 
 module.exports = blogRouter
