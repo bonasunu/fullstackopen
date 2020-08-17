c4e6a200f82f70dd71ee283a688afe2a30a50646
bonasunu <bonaventurasunu@gmail.com>
2020-08-17 13:19:03 +0700

    implement reduc on unicafe review

--- part6/unicafe/src/reducer.js
+++ part6/unicafe/src/reducer.js
@@ -8,12 +8,28 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
+      const changeStateGood = {
+        ...state,
+        good: state.good + 1
+      }
+      state = {...changeStateGood}
       return state
     case 'OK':
+      const changeStateOk = {
+        ...state,
+        ok: state.ok + 1
+      }
+      state = {...changeStateOk}
       return state
     case 'BAD':
+      const changeStateBad = {
+        ...state,
+        bad: state.bad + 1
+      }
+      state = {...changeStateBad}
       return state
     case 'ZERO':
+      state = {...initialState}
       return state
     default: return state
   }
