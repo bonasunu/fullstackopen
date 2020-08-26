df66b89b41ebf8c72eb5ec17de1c5054cd29f517
bonasunu <bonaventurasunu@gmail.com>
2020-08-26 14:58:52 +0700

    finish challenge 6.3

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -2,34 +2,30 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector((state) => state)
   const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    anecdotes.dispatch({
-      type: 'VOTE',
-      data: { id }
-    }) 
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button
+              onClick={() => dispatch({ type: 'VOTE', data: anecdote.id })}
+            >
+              vote
+            </button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
       <form>
-        <div><input /></div>
+        <div>
+          <input />
+        </div>
         <button>create</button>
       </form>
     </div>
