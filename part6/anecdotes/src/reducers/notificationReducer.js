6eea237f0d1566fea751f4b32ad6770013848291
bonasunu <bonaventurasunu@gmail.com>
2020-09-03 22:40:34 +0700

    edit timeout for notification

--- part6/anecdotes/src/reducers/notificationReducer.js
+++ part6/anecdotes/src/reducers/notificationReducer.js
@@ -10,10 +10,15 @@ const notificationReducer = (state = '', action) => {
   }
 }
 
+let timeout
+
 export const voteInfo = (content, time) => {
   return async (dispatch) => {
     await dispatch({ type: 'VOTE_INFO', data: content })
-    setTimeout(() => {
+
+    if (timeout) clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
       dispatch({
         type: 'REMOVE_INFO',
       })
